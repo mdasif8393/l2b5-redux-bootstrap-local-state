e5b17deb4bda79128940c790acb9a667ec12136f
mdasif8393 <md.asif.8393@gmail.com>
2025-07-04 11:48:55 +0600

    22-5 Initializing Shadcn UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import type { RootState } from "./redux/store";
-import {
-  decrement,
-  increment,
-  incrementByAmount,
-} from "./redux/features/counter/counterSlice";
-import { useAppDispatch, useAppSelector } from "./redux/hook";
+// import {
+//   decrement,
+//   increment,
+//   incrementByAmount,
+// } from "./redux/features/counter/counterSlice";
+
 import { Button } from "./components/ui/button";
 
 function App() {
-  const { count } = useAppSelector((state: RootState) => state.counter);
-  const dispatch = useAppDispatch();
+  // const { count } = useAppSelector((state: RootState) => state.counter);
+  // const dispatch = useAppDispatch();
   return (
     <>
       <div></div>
-      <h1>Counter: {count}</h1>
+      {/* <h1>Counter: {count}</h1> */}
       <div className="card">
         <Button>Button</Button>
-        <button onClick={() => dispatch(increment())}>Increment 1</button>
+        {/* <button onClick={() => dispatch(increment())}>Increment 1</button>
         <button onClick={() => dispatch(decrement())}>Decrement 1</button>
         <button onClick={() => dispatch(incrementByAmount(5))}>
           Increment By 5{" "}
-        </button>
+        </button> */}
       </div>
     </>
   );
